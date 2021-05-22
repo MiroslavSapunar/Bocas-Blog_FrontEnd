98b44162186e8970bf8a365c6e22a4ec793170f0
Miroslav <psapunar@fi.uba.ar>
2021-05-22 20:46:09 -0300

    analitics refactor

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -130,39 +130,30 @@ export default function Layout({ children }) {
     return (
         <div>
             <Head>
-                <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                        page_path: window.location.pathname,
-                    });
-                    `,
-                    }}
-                />
+
+                <meta httpEquiv="content-type" content="text/html; charset=UTF-8" />
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+                <meta lang="es" />
 
-                <title>Blog de Alimentación Consciente | Bocas Mercado Blog</title>
+                <title key="title">Blog de Alimentación Consciente | Bocas Mercado Blog</title>
                 <meta name="title" content="Blog de Alimentación Consciente | Bocas Mercado Blog" />
                 <meta name="description" key="description" content="Si ingresas vas a encontrar Datos, Consejos y Recomendaciones de Expertos sobre temáticas como Nutrición, Ecología, Meditación y Cuidado Personal." />
                 <meta name="keywords" content="bocas, mercado, expertos, nutricion, ecologia, meditacion, cuidado personal" />
 
-                <meta property="og:type" content="website" />
-                <meta property="og:site_name" content="Blog de Alimentación Consciente | Bocas Mercado Blog" />
                 <meta property="og:title" content="Blog de Alimentación Consciente | Bocas Mercado Blog" />
+                <meta property="og:site_name" content="Blog de Alimentación Consciente | Bocas Mercado Blog" />
                 <meta property="og:description" content="Si ingresas vas a encontrar Datos, Consejos y Recomendaciones de Expertos sobre temáticas como Nutrición, Ecología, Meditación y Cuidado Personal." />
-
-                <meta property="og:url" content="https://www.bocas.com.ar" />
-
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://blog.bocas.com.ar/" />
                 <meta property="og:image" content="http://d3ugyf2ht6aenh.cloudfront.net/stores/001/309/998/themes/common/logo-243132392-1601649288-ec5d9eaafdabc508ef3d2db5177acd451601649289.png?0" />
                 <meta property="og:image:secure_url" content="https://d3ugyf2ht6aenh.cloudfront.net/stores/001/309/998/themes/common/logo-243132392-1601649288-ec5d9eaafdabc508ef3d2db5177acd451601649289.png?0" />
 
                 <link rel="icon" href="/favicon.ico" />
 
+                <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+                <script src="/scripts/analytics.js" />
+            
             </Head>
             <body>
                 <header>
