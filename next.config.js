edebe6851e6871fe3e0e4c81542d396b3d938d1d
Miroslav <psapunar@fi.uba.ar>
2021-05-25 00:27:58 -0300

    next.config update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 // next.config.js
 module.exports = {
     images: {
-        domains: ["localhost", "127.0.0.1", "apis.blog.bocas.com.ar", "test.apis.blog.paginamiro.com.ar", "strapi", "strapi-dev"],
+        domains: ["localhost", "127.0.0.1", "apis.blog.bocas.com.ar", "test.api.blog.paginamiro.com.ar", "dev.api.blog.paginamiro.com.ar", "strapi", "strapi-dev"],
     },
 }
