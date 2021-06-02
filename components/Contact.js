7388e6cbbcfb4383888614f378771cece45a2082
Miroslav <psapunar@fi.uba.ar>
2021-06-01 21:59:28 -0300

    alerta email registrado

--- components/Contact.js
+++ components/Contact.js
@@ -1,4 +1,9 @@
+import { useState } from "react"
+
 function ContactComponent() {
+
+    const [exito, setExito] = useState(false)
+    const [error, setError] = useState(false)
 
     async function registerUser(event) {
         event.preventDefault()
@@ -15,57 +20,84 @@ function ContactComponent() {
                 'Authorization': `Bearer ` + process.env.NEXT_PUBLIC_ADMIN_JWT_SECRET,
             },
             method: 'POST'
-        }).catch(err => console.log(err))
-
-        console.log(res.status)
+        })
+            .then(res => {
+                if (res.statusCode != 200) {
+                    setError(true)
+                    setExito(false)
+                    console.log(res)
+                } else {
+                    setError(false)
+                    setExito(true)
+                }
+            })
+            .catch(err => console.log(err))
     }
+
+    const Formulario = () => (
+
+        <form onSubmit={registerUser}>
+
+            <div className="row">
+                <div className="col-md-12 form-group">
+                    <label style={{ marginBlock: ".75rem" }} htmlFor="fname">Nombre *</label>
+                    <input type="text" id="name" name="name" className="form-control form-control-lg" required />
+
+                    <label style={{ marginBlock: ".75rem" }} htmlFor="fsurname">Apellido *</label>
+                    <input type="text" id="surname" name="surname" className="form-control form-control-lg" required />
+
+                    <label style={{ marginBlock: ".75rem" }} htmlFor="eaddress">Email *</label>
+                    <input type="email" id="email" name="email" className="form-control form-control-lg" required />
+                    <small id="emailHelp" className="form-text text-muted">Nunca vamos a compartir tu email con terceros, ni solicitar informacion adicional</small>
+
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-md-12 form-group">
+                    <label style={{ marginBlock: ".75rem" }} htmlFor="message">Mensaje</label>
+                    <textarea name="" id="message" name="message" cols="30" rows="3" className="form-control"></textarea>
+                </div>
+            </div>
+
+            <div className="row justify-content-center">
+                <input type="submit" value="SUSCRIBIRSE" style={{ fontFamily: "Lato", fontStyle: "italic" }} className="btn btn-dark py-3 px-5" />
+            </div>
+
+
+
+        </form>
+    )
+
 
     return (
 
-        <div className="row w-75 justify-content-center justify-self-center py-5" >
+        <div className="row w-75 justify-content-center justify-self-center py-5" style={{ minHeight: "79vh" }}>
             <div className="col-lg-12">
                 <div className="section-title mb-3">
                     <h1>Bocas Newsletter</h1>
-                    <h4>¡No te pierdas de nada, suscribite a nuestro newsletter y mantenete al dia con las mejores notas, recetas y consejos en alimentacion saludable! ;)</h4>
+                    <h4>¡No te pierdas de nada, suscribite a nuestro newsletter y mantenete al dia con las mejores notas, recetas y consejos en alimentacion saludable!</h4>
                     <h6>(*) Campos obligatorios</h6>
                 </div>
-                <form onSubmit={registerUser}>
-                    <div className="row">
-                        <div className="col-md-12 form-group">
-                            <label htmlFor="fname">Nombre *</label>
-                            <input type="text" id="name" name="name" className="form-control form-control-lg" required />
 
-                            <label htmlFor="fsurname">Apellido *</label>
-                            <input type="text" id="surname" name="surname" className="form-control form-control-lg" required />
-
-                            <label htmlFor="eaddress">Email *</label>
-                            <input type="email" id="email" name="email" className="form-control form-control-lg" required />
-                            <small id="emailHelp" className="form-text text-muted">Nunca vamos a compartir tu email con terceros, ni solicitar informacion adicional</small>
-
-                        </div>
+                {error &&
+                    <div class="alert alert-danger my-3" role="alert">
+                        El email proporcionado ya esta registrado
                     </div>
-                    <div className="row">
-                        <div className="col-md-12 form-group">
-                            <label htmlFor="message">Mensaje</label>
-                            <textarea name="" id="message" name="message" cols="30" rows="10" className="form-control"></textarea>
-                        </div>
-                    </div>
+                }
 
-                    {/* <div class="form-group form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                    </div> */}
+                {!exito &&
+                    <Formulario />
+                }
 
-                    <div className="row justify-content-center">
+                {exito &&
+                    <h2>Gracias por suscribirte a nuestro newsletter ;)</h2>
+                }
 
-                        <input type="submit" value="SUSCRIBIRSE" className="btn btn-outline-dark py-3 px-5" />
-
-                    </div>
-
-                </form>
             </div>
 
         </div>
     )
+
 }
 export default ContactComponent
